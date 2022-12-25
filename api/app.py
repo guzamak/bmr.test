b5829c7bca0078340937de6d3e357c52e7383be1
พรวัฒนา วัฒนธำรงค์ <noipoke164@gmail.com>
2022-12-25 21:27:38 +0700

    2

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,url_for,jsonify,request
+from flask import Flask,render_template,jsonify,request
 from .my_package import calculator
 
 app = Flask(__name__)
